af79dc9d90aa985fc34942ca76d24876b853399d
mathieugandin <mathieu.gandin@appenin.fr>
2023-05-09 18:13:55 +0200

    tech: add test for flat built before 1945

--- src/domain/economical.influences/flat45/flat45.service.ts
+++ src/domain/economical.influences/flat45/flat45.service.ts
@@ -1,4 +1,5 @@
-import { Pool } from 'pg';
+import { poolWrapper } from '../../../config/database';
+import { PoolWrapper, StubbedPoolWrapper } from '../../../libs/pool-wrapper';
 
 interface FlatBuiltBefore1945 {
   insee_proportion_residence_principale_appart_avant_1945: number | null;
@@ -7,15 +8,15 @@ interface FlatBuiltBefore1945 {
 const flat45: string = 'insee_proportion_residence_principale_appart_avant_1945';
 
 export class Flat45Service {
-  constructor(private readonly database: Pool) {}
+  constructor(private readonly database: PoolWrapper) {}
 
   async getFlat45ByCoordinateLocation(lat: number, lon: number): Promise<FlatBuiltBefore1945> {
     const point = `ST_GeomFromText('POINT(${lon} ${lat})', 4326)::geometry`;
     const query = `
     (
       SELECT part_of_flat_built_before_1945::float as ${flat45}
-      FROM main_flat_residence_before_1945 
-      WHERE ST_Contains(geom::geometry, ${point}) 
+      FROM main_flat_residence_before_1945
+      WHERE ST_Contains(geom::geometry, ${point})
       union select 0 as ${flat45}
     ) order by ${flat45} desc limit 1;`;
 
@@ -26,5 +27,13 @@ export class Flat45Service {
     }
 
     return rows[0];
+  }
+
+  static create() {
+    return new Flat45Service(poolWrapper);
+  }
+
+  static createStubWith(flatBuiltBefore1945?: FlatBuiltBefore1945) {
+    return new Flat45Service(new StubbedPoolWrapper<FlatBuiltBefore1945>(flatBuiltBefore1945));
   }
 }
