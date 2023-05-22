b24f1f8079a2d9e36031d8bdfb2b5b8ed6b8385e
mathieugandin <mathieu.gandin@appenin.fr>
2023-05-22 15:58:30 +0200

    tech: separation of database url and api url in config and dot env files

--- src/config/serverConnection.ts
+++ src/config/serverConnection.ts
@@ -2,7 +2,7 @@ import { config } from './';
 
 export const pgisConfig = {
   port: config.FALCO_GEO_PORT,
-  host: config.FALCO_GEO_DB_HOST,
+  host: config.FALCO_GEO_HOST,
   routes: {
     cors: {
       origin: ['*'],
